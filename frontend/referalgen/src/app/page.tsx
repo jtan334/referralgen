638630e83e47868470ebf3c5039a78b3dc8f0277
jtan334 <jtan334@uwo.ca>
2024-09-01 23:40:10 -0400

    Set up, creating referal card

---
+++ frontend/referalgen/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import ReferalCard from "./components/ReferalCard";
+
+export default function Home() {
+  return (
+      <main>
+        <h1>Hello World</h1>
+        <ReferalCard/>
+      </main>
+  );
+}
